7fc6ea3e0f76c31f51a25d0b698b1ec8cffafb2e
huangxueqin <xhuuanniqege@gmail.com>
2019-12-28 17:46:01 +0800

    refactor io utils

--- app/src/main/java/com/xueqin/contacts/util/IOUtils.java
+++ app/src/main/java/com/xueqin/contacts/util/IOUtils.java
@@ -5,32 +5,37 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class IOUtils {
 
     public static String readTextFromAsset(@NonNull Context context,
                                            @NonNull String path) {
-        BufferedReader reader = null;
+        InputStream is = null;
         try {
-            reader = new BufferedReader(new InputStreamReader(context.getAssets().open(path)));
-            StringBuilder builder = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                builder.append(line);
-            }
-            return builder.toString();
+            is = context.getAssets().open(path);
+            return readTextFromStream(is);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            closeQuietly(reader);
+            closeQuietly(is);
         }
         return null;
+    }
+
+    @NonNull
+    public static String readTextFromStream(@NonNull InputStream is) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder builder = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+        }
+        return builder.toString();
     }
 
     public static void closeQuietly(@Nullable Closeable c) {
